0bb6ac3e56d1e72599abd5ea8169049f896975f2
Tim Jenness <t.jenness@jach.hawaii.edu>
2011-06-24 14:36:13 +0300

    Add a license file. GPL v3 has been approved by Tom Marsh

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+
+#    Copyright 2009-2011 Tom Marsh
+#    Copyright 2011 Tim Jenness
+#    All Rights Reserved.
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from distutils.core import setup, Extension
 
 import sys, os, subprocess, numpy
@@ -42,6 +60,6 @@ setup(name='starlink-pyndf',
       author_email='t.r.marsh@warwick.ac.uk',
       description='Python interface to Starlink ndf libraries',
       url='http://www.astro.warwick.ac.uk/',
-
+      license="GNU GPL v3",
       )
 
--- test/test_exceptions.py
+++ test/test_exceptions.py
@@ -15,3 +15,24 @@ class TestExceptions(unittest.TestCase):
     def test_badstate(self):
         with self.assertRaises(ValueError):
             indf,place = ndf.ndf_open('badmode', 'READ', 'VERYOLD')
+
+"""
+License
+=======
+
+Copyright 2011 Tim Jenness
+All Rights Reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
