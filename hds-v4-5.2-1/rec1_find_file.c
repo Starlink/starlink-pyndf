b12724b47376d7b0ae7ea34e61aae79fca101716
Sarah F. Graves <s.graves@eaobservatory.org>
2018-01-15 15:35:59 -1000

    hds/rec1_find_file.c: don't include config.h if HAVE_CONFIGH not set' @

--- hds-v4-5.2-1/rec1_find_file.c
+++ hds-v4-5.2-1/rec1_find_file.c
@@ -6,7 +6,6 @@
 void rec1_find_file( void ){};	 /* This routine not used on VMS systems */
 #else
 
-#include <config.h>              /* Autoconf definitions */
 
 /* C include files:							    */
 /* ===============							    */
