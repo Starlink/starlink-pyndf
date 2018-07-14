cd70c06d907a92d732205840030ff217494aae80
Sarah F. Graves <s.graves@eaobservatory.org>
2018-07-14 00:23:10 -1000

    test: make bash script runnable from other directories

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
+
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
+
 export HDS_VERSION=5
-pytest -v
+pytest -v $SCRIPTPATH
 export HDS_VERSION=4
-pytest -v
+pytest -v $SCRIPTPATH
 unset HDS_VERSION
