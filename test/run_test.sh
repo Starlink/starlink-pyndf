31c5d0419d2ff3f23c239ac518caf76e6c525951
Sarah F. Graves <s.graves@eaobservatory.org>
2018-07-15 19:23:26 -1000

    Ensure test script works on OSX and linux

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
 
-# Absolute path to this script. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f $0)
-# Absolute path this script is in. /home/user/bin
-SCRIPTPATH=`dirname $SCRIPT`
+# Relative path to this script.
+SCRIPTPATH=`dirname $0`
 
 export HDS_VERSION=5
 pytest -v $SCRIPTPATH
+
 export HDS_VERSION=4
 pytest -v $SCRIPTPATH
 unset HDS_VERSION
