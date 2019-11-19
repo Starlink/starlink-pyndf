5f34912f5b47412f595bfc8756835323daadc0d7
Sarah F. Graves <s.graves@eaobservatory.org>
2019-11-19 13:48:03 -1000

    Add extra include files from Starlink
    this is mostly files that are built during the starlink build process,
    but which a static copy from a recent built is fine to do. Move old
    ones from includefiles into this directory.

---
+++ missingincludes/ndf_cgen.h
@@ -0,0 +1,39 @@
+
+/* Now define the macros used by application code to invoke the appropriate
+   functions, depending on whether the old or new interface is required. 
+   Currently, there is no diffference betwen the two interfaces for the 
+   functions defined in this file but this may change in the future. */
+
+#define ndfGtszd ndfGtszD_
+#define ndfGtszr ndfGtszF_
+#define ndfGtszi ndfGtszI_
+#define ndfGtszw ndfGtszW_
+#define ndfGtszuw ndfGtszUW_
+#define ndfGtszb ndfGtszB_
+#define ndfGtszub ndfGtszUB_
+#define ndfGtszk ndfGtszK_
+#define ndfPtszd ndfPtszD_
+#define ndfPtszr ndfPtszF_
+#define ndfPtszi ndfPtszI_
+#define ndfPtszw ndfPtszW_
+#define ndfPtszuw ndfPtszUW_
+#define ndfPtszb ndfPtszB_
+#define ndfPtszub ndfPtszUB_
+#define ndfPtszk ndfPtszK_
+#define ndfXgt0d ndfXgt0D_
+#define ndfXgt0r ndfXgt0F_
+#define ndfXgt0i ndfXgt0I_
+#define ndfXgt0w ndfXgt0W_
+#define ndfXgt0uw ndfXgt0UW_
+#define ndfXgt0b ndfXgt0B_
+#define ndfXgt0ub ndfXgt0UB_
+#define ndfXgt0k ndfXgt0K_
+#define ndfXpt0d ndfXpt0D_
+#define ndfXpt0r ndfXpt0F_
+#define ndfXpt0i ndfXpt0I_
+#define ndfXpt0w ndfXpt0W_
+#define ndfXpt0uw ndfXpt0UW_
+#define ndfXpt0b ndfXpt0B_
+#define ndfXpt0ub ndfXpt0UB_
+#define ndfXpt0k ndfXpt0K_
+
