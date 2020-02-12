c319736137a8256ec8b607ed2ea21e67c9b3a211
Sarah F. Graves <s.graves@eaobservatory.org>
2020-02-11 16:49:01 -1000

    debugging travis build

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -e
 
+
+# Find python path
+pybinary=`which python`
+pydir=`dirname $pybinary`
+ls $pydir/../lib/python2.7/site-packages/starlink/
+
+
 # Relative path to this script.
 SCRIPTPATH=`dirname $0`
+echo $SCRIPTPATH
 
 export HDS_VERSION=5
 pytest -v $SCRIPTPATH
